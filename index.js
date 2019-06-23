db0f515741e63898d002a4b367cce5abc874f247
a.kovalenko <you@example.com>
2019-06-24 01:05:02 +0300

    Fix polling.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const http = require('http');
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
+const firebase = require('firebase');
 
 const token = process.env['tg_api_key'] || '892908707:AAGn1vOiAmYZH1Ddq315qoO8i_5bw30dPAk';
-const bot = new TelegramBot(token, { polling: true });
 const webHookUrl = 'https://awesome-777-bot.herokuapp.com';
 
 let port = process.env.PORT;
@@ -13,10 +13,22 @@ if (port == null || port === '') {
     port = 8000
 }
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBwobfTTiY794xNFCVavrzbOnkJMyFYq7A",
+    authDomain: "weatherbot-b16fb.firebaseapp.com",
+    databaseURL: "https://weatherbot-b16fb.firebaseio.com",
+    projectId: "weatherbot-b16fb",
+    storageBucket: "weatherbot-b16fb.appspot.com",
+    messagingSenderId: "419208145579",
+    appId: "1:419208145579:web:b38df2277851fe91"
+};
+
 require('http').createServer((req, res) => {
         res.end('Welcome to Awesome777Bot!')
     })
     .listen(port);
+
+const bot = new TelegramBot(token, { polling: true });
 
 const setWebHook = () => {
     const setWebhookUrl = `https://api.telegram.org/bot${token}/setWebhook`;
@@ -37,16 +49,32 @@ const setWebHook = () => {
 
 // setWebHook();
 
-bot.on('message', (msg) => {
-    console.log(msg);
-
-    request.on('error', function (error) {
-        console.error(error.status)
-    })
-});
+bot.startPolling();
+//
+// bot.on('message', (msg) => {
+//     console.log(msg.text);
+//     const chatId = msg.chat.id;
+//     bot.sendMessage(chatId,'Welcome to Awesome777Bot');
+//
+//     // request.on('error', function (error) {
+//     //     console.error(error.status)
+//     // })
+// });
 
 bot.onText('hi', function (msg, match) {
     console.log('Welcome to Awesome777Bot');
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Welcome to Awesome777Bot');
 });
 
-bot.on('polling_error', (error) => {});
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
+
+bot.on('polling_error', (error) => {
+    console.log(error)
+});
+
